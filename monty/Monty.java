3bc0fd8c463b3ce76036269191497bff5e6fd3d6
BogusKladik <boguskladik@gmail.com>
2023-04-13 01:55:30 +0300

    upgraded method with random

--- monty/Monty.java
+++ monty/Monty.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 
 public class Monty {
     public static void main(String[] args) {
-        int count_games;
+        int count_games = -1;
+        int count_doors = -1;
 
         Scanner console = new Scanner(System.in);
+
         do {
             System.out.print("Введите колличество игр: ");
             count_games = console.nextInt();
@@ -18,78 +20,100 @@ public class Monty {
 
             System.out.println("Неверное число, введите положительное число");
         } while (true);
+
+        do {
+            System.out.print("Введите колличество дверей: ");
+            count_doors = console.nextInt();
+
+            if (count_doors > 0) {
+                break;
+            }
+
+            System.out.println("Неверное число, введите положительное число");
+        } while (true);
+
         console.close();
 
-        Game game_with_change = new Game(count_games, true);
-        System.out.println("Число побед при изменении выбора: " + game_with_change.start_games());
+        Game game_with_change = new Game(count_games, count_doors, true);
+        System.out.println("Число побед при изменении выбора: " + game_with_change.run());
 
-        Game game_without_change = new Game(count_games, false);
-        System.out.println("Число побед без изменении выбора: " + game_without_change.start_games());
+        Game game_without_change = new Game(count_games, count_doors, false);
+        System.out.println("Число побед без изменении выбора: " + game_without_change.run());
     }
 }
 
 class Game {
-    Player player = new Player();
+    private final Player player;
+    private final int count_doors;
+    private final int count_games;
+    private final Random rand;
 
-    Random rand = new Random();
-
-    int count_games = 0;
+    public Game() {
+        this.player = new Player();
+        this.count_doors = 3;
+        this.count_games = 100;
+        this.rand = new Random();
+    }
 
     public Game(int count_games) {
+        this.player = new Player();
+        this.count_doors = 3;
         this.count_games = count_games;
+        this.rand = new Random();
     }
 
     public Game(int count_games, boolean player_is_smart) {
-        this(count_games);
-        this.player.smart = player_is_smart;
+        this.player = new Player(player_is_smart);
+        this.count_doors = 3;
+        this.count_games = count_games;
+        this.rand = new Random();
     }
 
-    public int start_games() {
+    public Game(int count_games, int count_doors) {
+        this.player = new Player();
+        this.count_doors = 3;
+        this.count_games = count_games;
+        this.rand = new Random();
+    }
+
+    public Game(int count_games, int count_doors, boolean player_is_smart) {
+        this.player = new Player(player_is_smart);
+        this.count_doors = count_doors;
+        this.count_games = count_games;
+        this.rand = new Random();
+    }
+
+    public int run() {
         int wins = 0;
-        for (int game = 0; game < count_games; game++) {
-            wins += start_game();
-        }
-        return wins;
-    }
 
-    private int start_game() {
-        int car_index = rand.nextInt(0, 3);
-        Door[] doors = new Door[3];
-        for (int door_index = 0; door_index < doors.length; door_index++) {
-            doors[door_index] = door_index == car_index ? Door.Car : Door.Goat;
-        }
+        for (int i = 0; i < count_games; i++) {
+            int car_door = rand.nextInt(count_doors);
+            player.selected_element = rand.nextInt(count_doors);
 
-        player.selected_element = rand.nextInt(0, 3);
+            int open_door = -1;
+            do {
+                open_door = rand.nextInt(count_doors);
+            } while (open_door == car_door || open_door == player.selected_element);
 
-        int[] possible_doors = init_possible_doors(doors.length, car_index, player.selected_element);
-
-        int index_door_with_goat = rand.nextInt(0, possible_doors.length);
-
-        if (player.smart) {
-            for (int door_index = 0; door_index < doors.length; door_index++) {
-                if (door_index != possible_doors[index_door_with_goat] && door_index != player.selected_element) {
-                    player.selected_element = door_index;
-                    break;
+            if (player.smart == false) {
+                if (player.selected_element == car_door) {
+                    wins++;
                 }
-            }
-        }
-
-        return doors[player.selected_element] == Door.Car ? 1 : 0;
-    }
-
-    int[] init_possible_doors(int length_doors, int car_index, int door_index) {
-        int size = car_index == door_index ? length_doors - 1 : length_doors - 2;
-
-        int[] possible_doors = new int[size];
-        int j = 0;
-        for (int possible_door = 0; possible_door < length_doors; possible_door++) {
-            if (possible_door == car_index || possible_door == door_index) {
                 continue;
             }
-            possible_doors[j++] = possible_door;
+
+            int switch_guess = -1;
+            do {
+                switch_guess = rand.nextInt(count_doors);
+            } while (switch_guess == open_door || switch_guess == player.selected_element);
+            player.selected_element = switch_guess;
+
+            if (player.selected_element == car_door) {
+                wins++;
+            }
         }
 
-        return possible_doors;
+        return wins;
     }
 }
 
@@ -109,9 +133,4 @@ class Player {
     public Player() {
         this(0, false);
     }
-}
-
-enum Door {
-    Goat,
-    Car
 }
